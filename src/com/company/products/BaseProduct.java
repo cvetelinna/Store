fb933fa5bea933f2a333dda4689ee34eb3beb80f
Tsvetelina Peycheva <cvety.peiceva@gmail.com>
2021-06-26 16:53:29 +0300

    Remove abstract method we do not need

--- src/com/company/products/BaseProduct.java
+++ src/com/company/products/BaseProduct.java
@@ -1,15 +1,35 @@
 package com.company.products;
 
+import com.company.enums.ProductTypeEnum;
+
 import java.math.BigDecimal;
 
 public abstract class BaseProduct {
+    private ProductTypeEnum type;
     private String name;
     private String brand;
     private BigDecimal price;
 
-    public abstract BigDecimal getDiscountedPrice();
+    public BaseProduct(ProductTypeEnum type, String name, String brand, BigDecimal price) {
+        this.type = type;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public ProductTypeEnum getType() {
+        return this.type;
+    }
 
     public BigDecimal getPrice() {
-        return price;
+        return this.price;
     }
 }
