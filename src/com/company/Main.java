296e3c1c22431241758b95ae067b1f3dd3d6c692
Tsvetelina Peycheva <cvety.peiceva@gmail.com>
2021-06-26 16:55:40 +0300

    Mock data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,72 @@
 package com.company;
 
+import com.company.distribution.CartItem;
+import com.company.enums.ClothingSizeEnum;
+import com.company.enums.ProductTypeEnum;
+import com.company.products.Appliance;
+import com.company.products.Clothing;
+import com.company.products.PerishableProduct;
+import com.company.products.ProductPrice;
+import com.company.services.CartService;
+import com.company.services.CashierService;
+import com.company.services.ProductService;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var apple = new PerishableProduct(
+                ProductTypeEnum.FOOD,
+                "apple",
+                "BrandA",
+                new BigDecimal("1.5"),
+                LocalDate
+                        .of(2021, 6, 14)
+                        .atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        var milk = new PerishableProduct(
+                ProductTypeEnum.FOOD,
+                "milk",
+                "BrandB",
+                new BigDecimal("0.99"),
+                LocalDate
+                        .of(2022, 2, 2)
+                        .atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        var clothing = new Clothing(
+                ProductTypeEnum.CLOTHING,
+                "t-shirt",
+                "BrandT",
+                new BigDecimal("15.99"),
+                ClothingSizeEnum.S,
+                "violet");
+
+        var appliance = new Appliance(
+                ProductTypeEnum.APPLIANCE,
+                "laptop",
+                "apple",
+                new BigDecimal("2345"),
+                new BigDecimal("1.5"),
+                "ModelL",
+                LocalDate
+                        .of(2020, 12, 2)
+                        .atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        var cartItems = new ArrayList<CartItem>();
+        cartItems.add(new CartItem(apple, new BigDecimal("2.45")));
+        cartItems.add(new CartItem(milk, new BigDecimal("3")));
+        cartItems.add(new CartItem(appliance,new BigDecimal("2")));
+        cartItems.add(new CartItem(clothing, new BigDecimal("2")));
+
+         var productService = new ProductService();
+         var cartService = new CartService(productService);
+         var cashierService = new CashierService(productService, cartService);
+
+         cashierService.printReceipt(cartItems, Instant.now());
     }
 }
